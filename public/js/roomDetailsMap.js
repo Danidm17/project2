0af587c15018365e79daaaf85d24d9b195e4c52a
Diego Rodriguez <godiedri@gmail.com>
2023-02-23 15:05:07 +0100

    depurar

--- public/js/roomDetailsMap.js
+++ public/js/roomDetailsMap.js
@@ -1,10 +1,9 @@
-// const solCoords = { lat: 40.417030047918736, lng: -3.70335134642059 }
+
+const solCoords = { lat: 40.417030047918736, lng: -3.70335134642059 }
 let myMap
 
-const IdRoom = document.querySelector('#id').value
-
 axios
-    .get(`/api/details/${IdRoom}`)
+    .get('/api/rooms')
     .then(({ data }) => setMarkers(data))
     .catch(err => console.log(err))
 
@@ -21,19 +20,58 @@ function initMap() {
     )
 }
 
-
 function setMarkers(rooms) {
+    rooms.forEach(elm => {
 
-    lat = rooms.location.coordinates[0]
-    lng = rooms.location.coordinates[1]
-    // myMap.setCenter({ lat, lng })
-    new google.maps.Marker({
-        map: myMap,
-        position: { lat, lng },
-        title: rooms.name
+        const lat = elm.location.coordinates[0]
+        const lng = elm.location.coordinates[1]
+
+        new google.maps.Marker({
+            map: myMap,
+            position: { lat, lng },
+            title: elm.name
+        })
     })
-
 }
+
+
+
+// // const solCoords = { lat: 40.417030047918736, lng: -3.70335134642059 }
+// let myMap
+
+// const IdRoom = document.querySelector('#id').value
+
+// axios
+//     .get(`/api/details/${IdRoom}`)
+//     .then(({ data }) => setMarkers(data))
+//     .catch(err => console.log(err))
+
+
+// function initMap() {
+
+//     myMap = new google.maps.Map(
+//         document.querySelector('#mapDetails'),
+//         {
+//             zoom: 16,
+//             center: solCoords,
+//             styles: mapStyles.retro
+//         }
+//     )
+// }
+
+
+// function setMarkers(rooms) {
+
+//     lat = rooms.location.coordinates[0]
+//     lng = rooms.location.coordinates[1]
+//     // myMap.setCenter({ lat, lng })
+//     new google.maps.Marker({
+//         map: myMap,
+//         position: { lat, lng },
+//         title: rooms.name
+//     })
+
+
 
 
 // function initMap() {
